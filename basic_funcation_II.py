259ba04f5b7fa72365e175d17f0847ae4f272836
elyassepasi <81493531+elyassepasi@users.noreply.github.com>
2021-05-09 19:28:25 -0700

    Add files via upload

---
+++ basic_funcation_II.py
@@ -0,0 +1,46 @@
+# def b(a):
+#     c=[]
+#     for i in range (a,-1,-1):
+#         c.append(i)
+#     return c
+# print (b(34))
+
+
+# def retun_print(a):
+    
+#     print (a[0])
+#     return a[1]
+
+# print (retun_print([2,3]))
+
+
+# def length_add (a):
+#     length=len(a)
+#     b= a[0]+length
+#     return b
+
+# print(length_add([5,9,5]))
+
+
+# def new_arr (a):
+#     max= a[1]
+#     maxarr=[]
+#     if len(a)>2:
+#         for i in range(1,len(a)):
+#             if i>max:
+#                 maxarr.append(i)
+#         print(len(maxarr))
+#         return maxarr
+#     else:
+#         return false
+
+# print(new_arr([5,2,3,2,1,4]))
+
+
+# def make_arr(a,b):
+#     arr=[]
+#     for i in range(a):
+#         arr.append(b)
+#     return(arr)
+
+# print(make_arr(4,7))
